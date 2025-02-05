8162d6bbde0e2ddeb09aff1c7cb7b066999a44b1
Zülal Özçelik <159713348+zulalozcelik@users.noreply.github.com>
2025-02-05 22:17:58 +0300

    05.02.2025

--- AcunMedyaRestaurantly/Controllers/CategoryController.cs
+++ AcunMedyaRestaurantly/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using AcunMedyaRestaurantly.Context;
+using AcunMedyaRestaurantly.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,5 +17,41 @@ namespace AcunMedyaRestaurantly.Controllers
             var value = Db.Categories.ToList();
             return View(value);
         }
+    
+        public ActionResult CategoryList()
+        {
+
+            var value = Db.Categories.ToList();
+            return View(value);
+        }
+        [HttpGet]
+        public ActionResult CategoryCreate()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CategoryCreate(Category model)
+        {
+            Db.Categories.Add(model);
+            Db.SaveChanges();
+            return RedirectToAction("CategoryList");
+        }
+
+        [HttpGet]
+        public ActionResult CategoryEdit(int id)
+        {
+            var value = Db.Categories.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public ActionResult CategoryEdit(Category model)
+        {
+            var values = Db.Categories.Find(model.CategoryId);
+            values.CategoryName = model.CategoryName;
+            Db.SaveChanges();
+            return RedirectToAction("CategoryList");
+        }
+
     }
 }
---
+++ AcunMedyaRestaurantly/Migrations/202502051913166_mig2createadmin.cs
@@ -0,0 +1,16 @@
+﻿namespace AcunMedyaRestaurantly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig2createadmin : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
